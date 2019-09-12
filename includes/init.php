309640929f90b624594475a45d5092a1b642785c
armandhansen1988 <55153420+armandhansen1988@users.noreply.github.com>
2019-09-12 11:14:31 +0200

    Update init.php

--- includes/init.php
+++ includes/init.php
@@ -4,9 +4,9 @@
 	DBC::init();
 	DBC::setConnectionID(1);
 	DBC::setServer('localhost');
-	DBC::setUser('armandco_mtc');
-	DBC::setPassword(');3%oDR)Hgn#');
-	DBC::setDatabase('armandco_mtc');
+	DBC::setUser('user');
+	DBC::setPassword('password');
+	DBC::setDatabase('database');
 	DBC::setCharset("utf8");
 	
 	date_default_timezone_set('Africa/Johannesburg');
